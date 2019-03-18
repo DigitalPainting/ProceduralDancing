29d52578aa4cbce618515e2c22711ff05c279851
Ross Gardler <rgardler@apache.org>
2019-03-17 22:06:10 -0700

    Group Puppet test assets together. Create a dancer spawner and create a simple demo scene.

---
+++ Assets/ProceduralDancing/Scripts/DancerSpawner.cs
@@ -0,0 +1,40 @@
+﻿using Puppet;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using wizardscode.environment;
+
+namespace wizardscode.procedural.dancers
+{
+    public class DancerSpawner : SimpleSpawner
+    {
+        internal override void CustomizeObject(GameObject go, SpawnableObject spawnerDefinition)
+        {
+            SpawnableDancer def = spawnerDefinition as SpawnableDancer;
+
+            var dancer = go.GetComponent<Puppet.Dancer>();
+
+            dancer.footDistance *= Random.Range(0.8f, 2.0f);
+            dancer.stepFrequency = Random.Range(def.minTempo, def.maxTempo);
+            dancer.stepHeight *= 0.1f + Random.Range(def.minEnergy, def.maxEnergy);
+            dancer.stepAngle *= Random.Range(def.minEnergy, def.maxEnergy);
+
+            dancer.hipHeight *= Random.Range(0.75f, 1.25f);
+            dancer.hipPositionNoise *= Random.Range(def.minEnergy, def.maxEnergy);
+            dancer.hipRotationNoise *= Random.Range(def.minEnergy, def.maxEnergy);
+
+            dancer.spineBend = Random.Range(def.minEnergy * 5, -def.maxEnergy * 12);
+            dancer.spineRotationNoise *= Random.Range(0.75f, 1.25f);
+
+            dancer.handPositionNoise *= Random.Range(0.5f, 2.0f);
+            dancer.handPosition += Random.insideUnitSphere * 0.25f;
+
+            dancer.headMove *= Random.Range(def.minEnergy / 2, def.maxEnergy * 2);
+            dancer.noiseFrequency *= Random.Range(0.4f, 1.8f);
+            dancer.randomSeed = Random.Range(0, 0xffffff);
+
+            var renderer = dancer.GetComponentInChildren<Renderer>();
+            renderer.material.color = Random.ColorHSV(0, 1, 0.6f, 0.8f, 0.8f, 1.0f);
+        }
+    }
+}
---
+++ Assets/ProceduralDancing/Scripts/ScriptableObjects/SpawnableDancer.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+namespace wizardscode.environment
+{
+    [CreateAssetMenu(fileName = "Spawnable Dancer", menuName = "Wizards Code/Environment/Spawnable Dancer")]
+    public class SpawnableDancer : SpawnableObject
+    {
+        [Header("Dancer configuration")]
+        [Tooltip("Minimum Tempo of the beat these dancers are dancing to. Make this the same as the maxTempo if you want your dancers to be perfectly in time.")]
+        [Range(0.3f, 1.4f)]
+        public float minTempo = 1f;
+        [Tooltip("Maximum Tempo of the beat these dancers are dancing to. Make this the same as the minTempo if you want your dancers to be perfectly in time.")]
+        [Range(0.3f, 1.4f)]
+        public float maxTempo = 1f;
+        [Tooltip("Minimum Energy of the dancers, the higher this is the more they will move. Make this the same as the maxEnergy if you want your dancers to be consistent.")]
+        [Range(0.75f, 1.25f)]
+        public float minEnergy = 1f;
+        [Tooltip("Maximum Tempo of the beat these dancers are dancing to. Make this the same as the minTempo if you want your dancers to be perfectly in time.")]
+        [Range(0.75f, 1.25f)]
+        public float maxEnergy = 1f;
+    }
+}
